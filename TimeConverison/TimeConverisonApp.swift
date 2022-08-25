9a56021bebd2201c0df3f0eec92b94066d9c52e3
hammam <hammam.a@munjz.com>
2022-08-25 21:03:15 +0300

    Initial Commit

---
+++ TimeConverison/TimeConverisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverisonApp.swift
+//  TimeConverison
+//
+//  Created by hammam abdulaziz on 27/01/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
